f38381c6ef39aa14d1488f04e0b08ecadd5b8a8c
Daniel Llewellyn <daniel@bowlhat.net>
2018-11-19 16:33:19 +0000

    update nodejs version from snap to 10/stable

--- .extra.d/perl.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "$HOME/perl5" ]; then
-	fetch_extract http://search.cpan.org/CPAN/authors/id/H/HA/HAARG/local-lib-2.000024.tar.gz /tmp
-	pushd /tmp/local-lib-2.000024
-	perl Makefile.PL --bootstrap
-	make test && make install
-	popd
-fi
-
-eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"
